637f22d08628712e7194779a2b380e2f08340d2f
saad <saadneosoft@gmail.com>
2023-08-09 18:06:01 +0530

    apical

--- Almofire/ViewController.swift
+++ Almofire/ViewController.swift
@@ -8,9 +8,6 @@
 //  "id": 3,
 //  "title": "fugiat veniam minus",
 //  "completed": false
-
-
-
 import UIKit
 import Alamofire
 struct details{
@@ -21,34 +18,14 @@ struct details{
 }
 class ViewController: UIViewController {
 
-    
-    @IBOutlet weak var postFirstname: UILabel!
-    
-    
+    @IBOutlet weak var latname: UILabel!
+    @IBOutlet weak var middlename: UILabel!
+    @IBOutlet weak var firstname: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-    var data :[String: Any] = [
-        "name" : "Saad" ,
-        "department" : "Mobile" ,
-        "team" : "Ios"
-    ]
     
-    
-    func postdata()
-    {
-        AF.request("https://dummy.restapiexample.com/api/v1/create" , method: .post , parameters: data ,encoding: JSONEncoding.default ).responseJSON { response in
-            switch response.result {
-                case .success(let value):
-                    // Handle the response data
-                    print("Response: \(value)")
-                case .failure(let error):
-                    // Handle the error
-                    print("Error: \(error)")
-                }
-        }
-    }
     func fetchdata()
     {
         AF.request("https://jsonplaceholder.typicode.com/todos/3").validate(statusCode: 200 ..< 300).responseJSON { response in
@@ -57,7 +34,7 @@ class ViewController: UIViewController {
                 self.firstname.isHidden = false
                 let json = value as! Dictionary<String , Any>
                 print(json["title"])
-                self.firstname.text = json["title"]! as? String
+                self.firstname.text = json["title"]! as! String
                 
             case .failure(let error):
                 print("Error: \(error)")
@@ -69,14 +46,11 @@ class ViewController: UIViewController {
     
     
     
-    @IBAction func postData(_ sender: UIButton) {
-        postdata()
-    }
+@IBAction func buttonAction(_ sender: UIButton) {
     
-    
-    @IBAction func Getdata(_ sender: UIButton) {
         fetchdata()
     }
+    
     
 
 }
